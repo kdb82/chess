a9a5c5d7272cac7dcd42229f3dbad179ca79bf2a
Kaden Bradshaw <kdnbradshaw@gmail.com>
2025-09-09 10:37:28 -0600

    Phase 0 starter changes

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -8,7 +8,12 @@ package chess;
  */
 public class ChessPosition {
 
+    private final int row;
+    private final int col;
+
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -26,4 +31,10 @@ public class ChessPosition {
     public int getColumn() {
         throw new RuntimeException("Not implemented");
     }
+
+    @Override
+    public String toString() {
+        return String.format("%d:%d", row, col);
+    }
+
 }
