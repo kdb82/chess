01a4a8b82bf38fb3d38896beec474c884cc249a0
Kaden Bradshaw <kdnbradshaw@gmail.com>
2025-10-22 15:13:58 -0600

    update memoryGameDao

--- server/src/main/java/dataaccess/MemoryGameDao.java
+++ server/src/main/java/dataaccess/MemoryGameDao.java
@@ -1,5 +1,31 @@
 package dataaccess;
 
-public class MemoryGameDao {
+import model.GameData;
 
+public class MemoryGameDao implements GameDao {
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void createGame(String gameName) {
+
+    }
+
+    @Override
+    public void listGames(String authToken) {
+
+    }
+
+    @Override
+    public GameData getGame(GameData gameData) {
+        return null;
+    }
+
+    @Override
+    public GameData updateGamePlayer(String gameID, String playerColor, String userName) {
+        return null;
+    }
 }
