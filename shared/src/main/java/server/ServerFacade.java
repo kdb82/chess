d35e1729e2214810b29dc236fc9baf3690bfe886
Kaden Bradshaw <kdnbradshaw@gmail.com>
2025-11-11 19:04:41 -0700

    fixed to include headers

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -18,59 +18,66 @@ public class ServerFacade {
 
     public RegisterResult register(RegisterRequest registerRequest) throws ResponseException {
         assert(registerRequest != null);
-        var request = buildRequest("POST", "/user", registerRequest);
+        var request = buildRequest("POST", "/user", registerRequest, null);
         var response = sendRequest(request);
         return handleResponse(response,  RegisterResult.class);
     }
 
     public LoginResult login(LoginRequest loginRequest) throws ResponseException {
         assert(loginRequest != null);
-        var request = buildRequest("POST", "/session", loginRequest);
+        var request = buildRequest("POST", "/session", loginRequest, null);
         var response = sendRequest(request);
         return handleResponse(response,  LoginResult.class);
     }
 
     public LogoutResult logout(LogoutRequest logoutRequest) throws ResponseException {
         assert(logoutRequest != null);
-        var request = buildRequest("POST", "/session", logoutRequest);
+        var token = logoutRequest.authToken();
+        var request = buildRequest("POST", "/session", logoutRequest, token);
         var response = sendRequest(request);
         return handleResponse(response,  LogoutResult.class);
     }
 
-    public CreateGameResult createGame(CreateGameRequest createGameRequest) throws ResponseException {
+    public CreateGameResult createGame(CreateGameRequest createGameRequest, String authToken) throws ResponseException {
         assert(createGameRequest != null);
-        var request = buildRequest("POST", "/game", createGameRequest);
+        var request = buildRequest("POST", "/game", createGameRequest, authToken);
         var response = sendRequest(request);
         return handleResponse(response,  CreateGameResult.class);
     }
 
-    public JoinGameResult joinGame(JoinGameRequest joinGameRequest) throws ResponseException {
+    public JoinGameResult joinGame(JoinGameRequest joinGameRequest, String authToken) throws ResponseException {
         assert(joinGameRequest != null);
-        var request = buildRequest("POST", "/game", joinGameRequest);
+        var request = buildRequest("POST", "/game", joinGameRequest, authToken);
         var response = sendRequest(request);
         return handleResponse(response,  JoinGameResult.class);
     }
 
     public ListGamesResult listGames(ListGameRequest listGameRequest) throws ResponseException {
         assert(listGameRequest != null);
-        var request = buildRequest("POST", "/game", listGameRequest);
+        var token = listGameRequest.authToken();
+        var request = buildRequest("POST", "/game", listGameRequest, token);
         var response = sendRequest(request);
         return handleResponse(response,  ListGamesResult.class);
     }
 
     public void clear() throws ResponseException {
-        var request = buildRequest("DELETE", "/db", null);
+        var request = buildRequest("DELETE", "/db", null, null);
         sendRequest(request);
     }
 
-    private HttpRequest buildRequest(String method, String path, Object body) {
-        var request = HttpRequest.newBuilder()
-                .uri(URI.create(serverUrl + path))
-                .method(method, makeRequestBody(body));
-        if (body != null) {
-            request.setHeader("Content-Type", "application/json");
+    private HttpRequest buildRequest(String method, String path, Object body, String authToken) {
+        var b = HttpRequest.newBuilder().uri(URI.create(serverUrl + path));
+        if (authToken != null && !authToken.isBlank()) {
+            b.header("Authorization", authToken);
         }
-        return request.build();
+        b.header("Accept", "application/json");
+        if (body != null) {
+            b.header("Content-Type", "application/json");
+        }
+        b.method(method, body == null
+                ? HttpRequest.BodyPublishers.noBody()
+                : HttpRequest.BodyPublishers.ofString(new Gson().toJson(body)));
+        return b.build();
     }
 
     private HttpRequest.BodyPublisher makeRequestBody(Object request) {
